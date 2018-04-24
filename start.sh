fbf7015713532c997bc42536bb31fcc26b6e2313
Brian Onang'o <surgbc@gmail.com>
2018-04-24 14:49:48 +0300

    update on start

--- start.sh
+++ start.sh
@@ -6,6 +6,8 @@ checktime=60 #1 hour	 #time in minutes after which to check for new software
 #times in seconds
 checktime=$((checktime * 60))
 
+git pull origin master
+
 while :			#continuously check for internet
 do
    {
